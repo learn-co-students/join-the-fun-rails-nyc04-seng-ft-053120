b07caa12fcdbc57ca0d26629ab63b33dce1afa06
21shield <21netaly@gmail.com>
2020-07-20 13:41:48 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,7 +1,10 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :passenger
+      t.belongs_to :taxi
       t.timestamps null: false
+
     end
   end
 end
